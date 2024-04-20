6857877f51320cece5ced2d75510efc916988f29
sarthaksarthak9 <sarthaknegi908@gmail.com>
2024-04-20 17:20:20 +0530

    main file

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const connectDB = require('./db/connect');
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+// routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+await mongoose.connect(process.env.MONGODB_URI);
+
+module.exports = {
+  app
+};
