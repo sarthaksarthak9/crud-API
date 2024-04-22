3dcf947076d39945805b352f07763e81c8c7e481
sarthaksarthak9 <sarthaknegi908@gmail.com>
2024-04-22 12:10:15 +0530

    update

--- tests/product.test.js
+++ tests/product.test.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 const request = require("supertest");
+const Product = require("../models/product.model");
 const app = require("../app");
 
 /* Connecting to the database before each test. */
@@ -20,50 +21,125 @@ describe("GET /api/products", () => {
       expect(res.statusCode).toBe(200);
       expect(res.body.length).toBeGreaterThan(0);
     });
-  });
+
+    it("should return 500 if an error occurs", async () => {
+      // Force an error by not connecting to the database
+      await mongoose.disconnect();
+
+      const res = await request(app).get("/api/products");
+
+      expect(res.statusCode).toBe(500);
+      expect(res.body).toHaveProperty("message");
+    });
+});
 
 describe("GET /api/products/:id", () => {
     it("should return a product", async () => {
       const res = await request(app).get(
-        "/api/products/661fdd741595e378925b0879"
+        "/api/products/6623bca25d5fde658a92acca"
       );
       expect(res.statusCode).toBe(200);
       expect(res.body.name).toBe("airpods");
     });
-  });
+
+    it("should return 500 if an error occurs", async () => {
+      // Disconnect from the database to force an error
+      await mongoose.disconnect();
+
+      const res = await request(app).get("/api/products/6623bca25d5fde658a92acca");
+
+      expect(res.statusCode).toBe(500);
+      expect(res.body).toHaveProperty("message");
+    });
+});
   
 describe("POST /api/products", () => {
     it("should create a product", async () => {
-      const res = await request(app).post("/api/products").send({
-        name: "airpods",
-        quantity : 2,
-        price: 200,
-      });
+      const res = await request(app)
+        .post("/api/products")
+        .send({
+          name : "iPad",
+          quantity : 4,
+          price : 20000,
+        });
+      
       expect(res.statusCode).toBe(200);
-      expect(res.body.name).toBe("airpods");
+      expect(res.body.quantity).toBe(4);
     });
-  });
+    
+    it("should return 500 if an error occurs", async () => {
+      // Disconnect from the database to force an error
+      await mongoose.disconnect();
+
+      const res = await request(app)
+        .post("/api/products")
+        .send({
+          name: "iPad",
+          quantity : 4,
+          price: 20000,
+        });
+
+      expect(res.statusCode).toBe(500);
+      expect(res.body.name).toBe("iPad");
+    });
+});
+
   
 describe("PUT /api/products/:id", () => {
-    it("should update a product", async () => {
+  it("should update a product", async () => {
+      // Insert a sample product into the database
+      // const sampleProduct = await Product.create({ name: "ZenZ", quantity: 10, price: 100 });
+
+      const updatedProductData = {
+          name: "Updated Product",
+          quantity: 20,
+          price: 200
+      };
+
       const res = await request(app)
-        .patch("/api/products/661fdd741595e378925b0879")
-        .send({
-          name: "airpods",
-          quantity : 2,
-          price: 200,          
-        });
+          .patch("/api/products/662399b3e24d0c5ecc531e0d")
+          .send(updatedProductData);
+
       expect(res.statusCode).toBe(200);
-    });
+      expect(res.body.name).toBe("Updated Product");
   });
+
+  it("should return 404 if product ID does not exist", async () => {
+      const res = await request(app)
+          .patch("/api/products/6068b2447917f4b3f089cc35")
+          .send({ name: "Updated Product" });
+
+      expect(res.statusCode).toBe(404);
+  });
+});
   
 describe("DELETE /api/products/:id", () => {
     it("should delete a product", async () => {
       const res = await request(app).delete(
-        "/api/products/661fdd741595e378925b0879"
+        "/api/products/6623bb20918f9d2584a9e056"
       );
       expect(res.statusCode).toBe(200);
     });
-  });
+
+    it("should return 404 if product ID does not exist", async () => {
+     
+      const res = await request(app).delete("/api/products/6068b2447917f4b3f089cc35");
+
+      // Assertions
+      expect(res.statusCode).toBe(404);
+      expect(res.body).toHaveProperty("message", "Product not found");
+    });
+
+    it("should return 500 if an error occurs", async () => {
+        // Disconnect from the database to force an error
+        await mongoose.disconnect();
+
+        const res = await request(app).delete("/api/products/6623bb20918f9d2584a9e056");
+
+        // Assertions
+        expect(res.statusCode).toBe(500);
+        expect(res.body).toHaveProperty("message");
+    });
+});
 
   
